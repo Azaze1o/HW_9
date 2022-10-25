7751f93f3510ced7ee8b0fca1b799fbb0d5a172b
Илья <gagarinila@yandex.ru>
2022-10-25 17:50:05 +0500

    Create 3 tests

---
+++ src/test/java/demo/qa/BaseTest.java
@@ -0,0 +1,22 @@
+package demo.qa;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+import io.qameta.allure.selenide.AllureSelenide;
+
+public class BaseTest {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen=true;
+
+    }
+
+}
